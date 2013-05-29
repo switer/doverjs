851234d652786f0c11a9bbffa45f38460d0ca2d3
guanks <guankaishe@gmail.com>
2013-05-29 17:53:58 +0800

    add remove api ref

--- examples/test.js
+++ examples/test.js
@@ -1,9 +1,9 @@
 var dover = require('doverjs');
-dover.cover({
+dover.remove({
 	html : "http://www.baidu.com",
-	style : "cv_style.css"
-}, function (results, outputs) {
-	console.log(results)
+	style : ["cv_style.css"]
+}, function (resp) {
+	console.log(resp)
 }, function (err) {
 	console.log(err);
 });
--- tools/dover.js
+++ tools/dover.js
@@ -1,6 +1,51 @@
-var parser = require('../lib/parser.js');
-var localPath  = process.cwd().replace(/^\'/,'').replace(/\'$/,'');
-function cover(params, success, error) {
+var parser 	= require('../lib/parser.js'),
+	_ 		= require('../lib/underscore-min.js'),
+	loader 	= require('../lib/loader.js'),
+	util 	= require('../lib/util.js'),
+	colors 	= require('color'),
+	fs 		= require('fs');
+
+
+var localPath  = process.cwd().replace(/^\'/,'').replace(/\'$/,''),
+	rmfilePrefix = 'dover_';
+function cover (params, success, error) {
 	parser.cover(params, localPath, success, error)
 }
+
+function remove (params, success, error) {
+	var count = 0,
+		rmFiles = [];
+	cover(params, function (resp) {
+		for (var i = resp.length - 1; i >= 0; i--) {
+			var data = resp[i],
+				uri	 = data.uri,
+				unusedSels = data.results.unused,
+				fullURI = util.populateLocalURL(uri, localPath);
+
+			loader.loadResource(fullURI, function (uriCtns) {
+				_.each(uriCtns, function (item) {
+					var ctn = item.content,
+						fileName = rmfilePrefix + uri;
+					_.each(unusedSels, function (sel) {
+						ctn = ctn.replace(new RegExp(util.encode(sel) + '\s*\{[^\}\{]*\}', 'g'), '')
+					})
+					rmFiles.push({
+						uri : uri,
+						content : ctn,
+						results : data.results
+					});
+					// fs.writeFileSync(fileName, ctn, 'UTF-8');
+					// console.log('Remove unused selectors and write file : '.yellow + fileName);
+				});
+				count ++;
+				if (count >= resp.length) {
+					// console.log('Remove compelete!'.green)
+					success && success(rmFiles);
+				}
+
+			}, error);
+		};
+	}, error)
+}
 exports.cover = cover;
+exports.remove = remove;
